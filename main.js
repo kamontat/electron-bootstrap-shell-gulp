87c642edade8ddfe46440f61df6c65ad34ad41ff
kamontat <kamontat_c@hotmail.com>
2017-05-30 23:05:06 +0700

    update to electron 1.6.2

--- main.js
+++ main.js
@@ -1,16 +1,12 @@
-
-var app = require('app');  // Module to control application life.
-var BrowserWindow = require('browser-window');  // Module to create native browser window.
-
-// Report crashes to our server.
-require('crash-reporter').start();
+const electron = require('electron')
+const { app, BrowserWindow } = electron
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 var mainWindow = null;
 
 // Quit when all windows are closed.
-app.on('window-all-closed', function() {
+app.on('window-all-closed', function () {
   // On OS X it is common for applications and their menu bar
   // to stay active until the user quits explicitly with Cmd + Q
   if (process.platform != 'darwin') {
@@ -20,18 +16,18 @@ app.on('window-all-closed', function() {
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
-app.on('ready', function() {
+app.on('ready', function () {
   // Create the browser window.
-  mainWindow = new BrowserWindow({width: 800, height: 600+410});
+  mainWindow = new BrowserWindow({ width: 800, height: 600 + 410 });
 
   // and load the index.html of the app.
-  mainWindow.loadUrl('file://' + __dirname + '/index.html');
+  mainWindow.loadURL('file://' + __dirname + '/index.html');
 
   // Open the DevTools.
   mainWindow.openDevTools();    // requires a height 410px 
 
   // Emitted when the window is closed.
-  mainWindow.on('closed', function() {
+  mainWindow.on('closed', function () {
     // Dereference the window object, usually you would store windows
     // in an array if your app supports multi windows, this is the time
     // when you should delete the corresponding element.
--- test.sh
+++ test.sh
@@ -1,39 +1,38 @@
 #!/bin/bash
 
-echo "Hello"
+printf "Hello"
 
 sleep 10
 
-echo "Hi There"
+printf "Hi There"
 
 sleep 5
 
-echo "This is a long long text.....asdasdasdasdasdasdadadadasdadadadadasdadadadadadadasd"
-echo "Hello there thisi a test ogf the emergency broadcast system, if this were a real emergency you problably wouldnt be watching tv."
-
-
-sleep 0.5
-echo "hello 1"
+printf "This is a long long text.....asdasdasdasdasdasdadadadasdadadadadasdadadadadadadasd"
+printf "Hello there thisi a test ogf the emergency broadcast system, if this were a real emergency you problably wouldnt be watching tv."
 
 sleep 0.5
-echo "hello 2"
+printf "hello 1"
 
 sleep 0.5
-echo "hello 3"
+printf "hello 2"
 
 sleep 0.5
-echo "hello 4"
+printf "hello 3"
 
 sleep 0.5
-echo "hello 5"
+printf "hello 4"
 
 sleep 0.5
-echo "hello 6"
+printf "hello 5"
 
 sleep 0.5
-echo "hello 7"
+printf "hello 6"
 
 sleep 0.5
-echo "hello 8"
+printf "hello 7"
+
+sleep 0.5
+printf "hello 8"
 
 
